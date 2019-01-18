f5b4ccbea21cbbfb69f4cb4f7b9b24fdd9755c4f
JustinSHong <jhong2009@gmail.com>
2019-01-17 22:35:38 -0800

    added function exercises

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -145,3 +145,40 @@ for i in 0..<10 {
     count += i
 }
 print(count)
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+print(greet(person: "Michael", day: "Monday"))
+print(greet(person: "Justin", day: "Friday"))
+print(greet(person: "Taliyah", day: "Tuesday"))
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.max)
+print(statistics.min)
+
+func addFive() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+print(addFive())
