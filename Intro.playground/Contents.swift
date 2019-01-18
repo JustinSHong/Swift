24dbf9240da6fe762e7e27fd6078346a6195e573
JustinSHong <jhong2009@gmail.com>
2019-01-17 22:26:43 -0800

    added looping exercises

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -108,3 +108,40 @@ switch vegetable {
 default:
     print("Put it in a soup!")
 }
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25]
+]
+print(interestingNumbers)
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if (number > largest) {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+var n = 2
+while n < 100 {
+    print(n)
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    print(m)
+    m *= 2
+} while m < 100
+print(m)
+
+var count = 0
+for i in 0..<10 {
+    print(i)
+    count += i
+}
+print(count)
